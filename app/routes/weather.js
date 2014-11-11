41d61d44085e4caf46e30fc5efb6409ff7b00d56
Maxwell Vance <max.vance@gmail.com>
2014-11-11 00:38:03 -0600

    server-side weather-getting

---
+++ app/routes/weather.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var request = require('request');
+
+exports.getWeather = function(req, res){
+  var key = process.env.WUNDERGROUND;
+  var zip = req.body.zipCode;
+  if (key === undefined){
+    throw new Error('WUNDERGROUND key undefined! Did you remember to define it at runtime or export it in your .bash_profile?');
+  }
+  var url = 'http://api.wunderground.com/api/' + key + '/conditions/q/' + zip + '.json?callback=?';
+  console.log('URL for wunderground API: ' + url);
+  request.post(url, function(err, response, body){
+    res.send(body);
+  });
+};
+
