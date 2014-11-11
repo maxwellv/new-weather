646005d918536683b5ae2249cf057b3168ed9323
Maxwell Vance <max.vance@gmail.com>
2014-11-10 23:48:47 -0600

    raw main.js functionality

--- app/static/js/source/main.js
+++ app/static/js/source/main.js
@@ -6,6 +6,23 @@
 
   function initialize(){
     $(document).foundation();
+    $('#get-weather').click(getWeather);
+  }
+
+  function getWeather(){
+    var url = makeURL();
+    url +='/getWeather';
+    var zip = $('#ZIP').val();
+    //TODO: regex to see if this is an actual zip code
+    console.log('ZIP code is ' + zip);
+    $.ajax({url:url, type:'POST', data:{zipCode:zip}, success:function(data){
+      console.log(data);
+    }});
+  }
+
+  function makeURL(){
+    var url = window.location.origin.replace(/[0-9]{4}/, '4000');
+    return url;
   }
 
 })();
