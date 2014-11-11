4b75d2e3f88ff7ad436762c002453cbc0f5638d6
Maxwell Vance <max.vance@gmail.com>
2014-11-11 00:40:55 -0600

    cut out unnecessary console log

--- app/routes/weather.js
+++ app/routes/weather.js
@@ -9,7 +9,6 @@ exports.getWeather = function(req, res){
     throw new Error('WUNDERGROUND key undefined! Did you remember to define it at runtime or export it in your .bash_profile?');
   }
   var url = 'http://api.wunderground.com/api/' + key + '/conditions/q/' + zip + '.json?callback=?';
-  console.log('URL for wunderground API: ' + url);
   request.post(url, function(err, response, body){
     res.send(body);
   });
